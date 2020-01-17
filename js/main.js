add927d083761c77c366bd1a9ee504928cadcf09
vikytrusowa <vikytrusowa.@gmail.com>
2020-01-17 21:34:20 +0200

    add adaptive section

--- js/main.js
+++ js/main.js
@@ -48,30 +48,48 @@ var marker = new google.maps.Marker({
 });
 
 // 1. Получить ответ гугл капчи
-var captcha = grecaptcha.getResponse();
+// var captcha = grecaptcha.getResponse();
+//
+// // 2. Если ответ пустой, то выводим сообщение о том, что пользователь не прошёл тест.
+// // Такую форму не будем отправлять на сервер.
+// if (!captcha.length) {
+//     // Выводим сообщение об ошибке
+//     $('#recaptchaError').text('* Вы не прошли проверку "Я не робот"');
+// } else {
+//     // получаем элемент, содержащий капчу
+//     $('#recaptchaError').text('');
+// }
+//
+// // 3. Если форма валидна и длина капчи не равно пустой строке, то отправляем форму на сервер (AJAX)
+// if ((formValid) && (captcha.length)) {
+//     // добавить в formData значение 'g-recaptcha-response'=значение_recaptcha
+//     formData.append('g-recaptcha-response', captcha);
+//
+// }
+//
+// // 4. Если сервер вернул ответ error, то делаем следующее...
+// // Сбрасываем виджет reCaptcha
+// grecaptcha.reset();
+// // Если существует свойство msg у объекта $data, то...
+// if ($data.msg) {
+//     // вывести её в элемент у которого id=recaptchaError
+//     $('#recaptchaError').text($data.msg);
+// }
+function changeCapthaSize() {
+    var reCaptchaWidth = 302;
+    var containerWidth = $('.reach__captcha').width();
+    if(reCaptchaWidth > containerWidth) {
+        var reCaptchaScale = containerWidth / reCaptchaWidth;
+        $('.g-recaptcha').css({
+'transform':'scale(' +reCaptchaScale+ ')',
+    'transform-origin':'left top'
+    });
+    }
+};
 
-// 2. Если ответ пустой, то выводим сообщение о том, что пользователь не прошёл тест.
-// Такую форму не будем отправлять на сервер.
-if (!captcha.length) {
-    // Выводим сообщение об ошибке
-    $('#recaptchaError').text('* Вы не прошли проверку "Я не робот"');
-} else {
-    // получаем элемент, содержащий капчу
-    $('#recaptchaError').text('');
-}
-
-// 3. Если форма валидна и длина капчи не равно пустой строке, то отправляем форму на сервер (AJAX)
-if ((formValid) && (captcha.length)) {
-    // добавить в formData значение 'g-recaptcha-response'=значение_recaptcha
-    formData.append('g-recaptcha-response', captcha);
-
-}
-
-// 4. Если сервер вернул ответ error, то делаем следующее...
-// Сбрасываем виджет reCaptcha
-grecaptcha.reset();
-// Если существует свойство msg у объекта $data, то...
-if ($data.msg) {
-    // вывести её в элемент у которого id=recaptchaError
-    $('#recaptchaError').text($data.msg);
-}
+$(window).ready(function(){
+    changeCapthaSize();
+});
+$(window).resize(function(){
+    changeCapthaSize();
+});
