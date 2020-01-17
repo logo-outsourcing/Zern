c7331f1e01c5d198a408e61a4eb06da612ee1a9a
vikytrusowa <vikytrusowa.@gmail.com>
2020-01-17 15:24:31 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+$('.banner__slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slide: "div",
+    dotsClass: ' slick-dots banner__dots',
+    prevArrow: ".banner__prev",
+    nextArrow: ".banner__next",
+});
+
+var map = $('.reach__map')[0];
+var uluru = {lat: 49.5687689, lng: 34.5858791};
+var map1 = new google.maps.Map(map,
+    {
+        center: uluru,
+        zoom: 3
+    });
+var pos1 = new google.maps.LatLng(50.4374807, 30.6095538);
+var pos2 = new google.maps.LatLng(-4.0101626, 17.2339139);
+var pos3 = new google.maps.LatLng(4.6420843, -78.8351989);
+var pos4 = new google.maps.LatLng(15.7583519, 101.4151049);
+var marker = new google.maps.Marker({
+    position: pos1,
+    map: map1,
+    title: '',
+    icon: "img/home/marker.svg"
+
+
+});
+var marker = new google.maps.Marker({
+    position: pos2,
+    map: map1,
+    icon: "img/home/marker.svg",
+    title: 'Кленовая Улица д.2'
+});
+var marker = new google.maps.Marker({
+    position: pos3,
+    map: map1,
+    icon: "img/home/marker.svg",
+    title: 'Кленовая Улица д.2'
+});
+var marker = new google.maps.Marker({
+    position: pos4,
+    map: map1,
+    icon: "img/home/marker.svg",
+    title: 'Кленовая Улица д.2'
+});
+
+// 1. Получить ответ гугл капчи
+var captcha = grecaptcha.getResponse();
+
+// 2. Если ответ пустой, то выводим сообщение о том, что пользователь не прошёл тест.
+// Такую форму не будем отправлять на сервер.
+if (!captcha.length) {
+    // Выводим сообщение об ошибке
+    $('#recaptchaError').text('* Вы не прошли проверку "Я не робот"');
+} else {
+    // получаем элемент, содержащий капчу
+    $('#recaptchaError').text('');
+}
+
+// 3. Если форма валидна и длина капчи не равно пустой строке, то отправляем форму на сервер (AJAX)
+if ((formValid) && (captcha.length)) {
+    // добавить в formData значение 'g-recaptcha-response'=значение_recaptcha
+    formData.append('g-recaptcha-response', captcha);
+
+}
+
+// 4. Если сервер вернул ответ error, то делаем следующее...
+// Сбрасываем виджет reCaptcha
+grecaptcha.reset();
+// Если существует свойство msg у объекта $data, то...
+if ($data.msg) {
+    // вывести её в элемент у которого id=recaptchaError
+    $('#recaptchaError').text($data.msg);
+}
